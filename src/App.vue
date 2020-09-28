98542dd73c7621c50763055a2301e0d83ee814c6
Marc de Ruijter <marc@dizid.com>
2020-09-28 13:11:27 +0200

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <searchresults />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import searchresults from "./components/SearchResults.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    searchresults
   }
 };
 </script>
---
+++ src/components/SearchResults.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div class="gcse-searchresults-only"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
